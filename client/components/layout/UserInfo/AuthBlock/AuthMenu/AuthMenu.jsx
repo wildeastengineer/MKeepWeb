2145f29b741c3721c75b9064adeeb48630e724fc
WEE <wildeastengineer@gmail.com>
2017-05-30 09:17:02 +0400

    Trello-5BfCYYpD. Get PropTypes from separate lib

--- client/components/layout/UserInfo/AuthBlock/AuthMenu/AuthMenu.jsx
+++ client/components/layout/UserInfo/AuthBlock/AuthMenu/AuthMenu.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { TextInput, Button } from 'components/common';
 
 if (process.env.BROWSER) {
