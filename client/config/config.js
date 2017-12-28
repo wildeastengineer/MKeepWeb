ddda6c85e339f5f0f110d0f0932889873c2bf78b
WEE <wildeastengineer@gmail.com>
2017-12-28 08:52:41 +0400

    Temp config stub

--- client/config/config.js
+++ client/config/config.js
@@ -1,14 +1,14 @@
 //ToDo: Replace to async config loading according to environment
 
-import dev from './config.dev';
+//import dev from './config.dev';
 import prod from './config.prod';
 
 /* eslint-disable no-alert, no-process-env, no-undef */
 const isBuilding = !!process.env.BUILDING;
-const isProduction = process.env.NODE_ENV === 'production';
+//const isProduction = process.env.NODE_ENV === 'production';
 /* eslint-enable no-alert, no-process-env, no-undef */
 
-const currentEnvConf = isProduction ? prod : dev;
+const currentEnvConf = prod;
 
 export default Object.assign({
     isBuilding,
